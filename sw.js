00df6d65c72e635c5a694fd0e72a2107bf91e1be
pabloxmoreno <pabloxxxmoreno@gmail.com>
2026-04-17 17:28:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,31 @@
-const CACHE_NAME = 'workout-tracker-v1';
+const CACHE_NAME = 'workout-tracker-v2'; // Zmieniono wersję, aby wymusić aktualizację
+
+// Używamy bezwzględnych ścieżek zaczynających się od /
 const ASSETS = [
-  './',
-  './index.html',
-  './css/styles.css',
-  './js/app.js',
-  './js/config.js',
-  './js/store.js',
-  './js/ui.js',
-  './js/utils.js',
-  './manifest.json'
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/app.js',
+  '/js/config.js',
+  '/js/store.js',
+  '/js/ui.js',
+  '/js/utils.js',
+  '/manifest.json'
 ];
 
-// Instalacja: zapisz pliki w cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(err => {
+        console.warn('Nie udało się dodać niektórych plików do cache:', err);
+        // Kontynuuj mimo błędu pojedynczego pliku
+        return Promise.resolve(); 
+      });
+    })
   );
   self.skipWaiting();
 });
 
-// Aktywacja: wyczyść stare cache
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => Promise.all(
@@ -31,7 +37,6 @@ self.addEventListener('activate', (e) => {
   self.clients.claim();
 });
 
-// Pobieranie: serwuj z cache, jeśli brak sieci
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
